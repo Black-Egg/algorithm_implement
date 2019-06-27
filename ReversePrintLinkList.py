882d5c24049331fe9c47518d1498cbb2e235fa13
yeanjiang <1252980880@qq.com>
2019-06-27 11:37:48 +0800

    Add the answers of questions 3 and 4.

--- ReversePrintLinkList.py
+++ ReversePrintLinkList.py
@@ -1,1 +1,22 @@
-def
+# -*- coding:utf-8 -*-
+class ListNode:
+    def __init__(self, x, listNode):
+        self.val = x
+        self.next = listNode
+
+class Solution:
+    # 返回从尾部到头部的列表值序列，例如[1,2,3]
+    def printListFromTailToHead(self, listNode):
+        # write code here
+        if listNode is None:
+            return []
+        else:
+            return self.printListFromTailToHead(listNode.next) + [listNode.val]
+            
+
+if __name__ == "__main__":
+    nodeList = ListNode(0, None)
+    for i in range(1,10):
+        nodeList = ListNode(i, nodeList)
+    sol = Solution()
+    print(sol.printListFromTailToHead(nodeList))
