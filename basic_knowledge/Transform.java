8d3115cf0f960e1a34e322f2adfb3f8d6191da8d
yeanjiang <yeanjiang11@qq.com>
2019-10-08 11:23:20 +0800

    update

--- basic_knowledge/Transform.java
+++ basic_knowledge/Transform.java
@@ -1,6 +1,9 @@
 import java.util.Arrays;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
  
 public class Main {
     public static void main(String[] args) {
@@ -38,13 +41,34 @@ public class Main {
         // Integer[] 转 List<Integer>
         List<Integer> list2 = Arrays.asList(integers1);
         // 最简单的方式。String[]转List<String>也同理。
- 
+
+
+        // int[] 转 Map<Integer>
+        Set<Integer> set1 = Arrays.stream(data).boxed().collect(Collectors.toSet());
+        // Set<Integer> set1 = IntStream.of(data).boxed().collect(Collectors.toSet());
+        // Arrays.stream(arr) 可以替换成IntStream.of(arr)。
+        // 1.使用Arrays.stream将int[]转换成IntStream。
+        // 2.使用IntStream中的boxed()装箱。将IntStream转换成Stream<Integer>。
+        // 3.使用Stream的collect()，将Stream<T>转换成Set<T>，因此正是Set<Integer>。
+
+        // Map<Integer> 转 int[]
+        int[] arr3 = set1.stream().mapToInt(Integer::valueOf).toArray();
+        // 想要转换成int[]类型，就得先转成IntStream。
+        // 这里就通过mapToInt()把Stream<Integer>调用Integer::valueOf来转成IntStream
+        // 而IntStream中默认toArray()转成int[]。
+
+
         // 同理
         String[] strings1 = {"a", "b", "c"};
         // String[] 转 List<String>
         List<String> list3 = Arrays.asList(strings1);
         // List<String> 转 String[]
         String[] strings2 = list3.toArray(new String[0]);
+        // String[] 转 Map<String>
+        Set<String> set2 = new HashSet<String>(Arrays.asList(strings1));
+        // Map<String> 转 String[]
+        String[] strings3 = new String[set2.size()];     
+        set2.toArray(strings3);
  
     }
 }
