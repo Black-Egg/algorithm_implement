6b19b9dfab66430a86fd12265e44317ffc8b16dc
yaj <1252980880@qq.com>
2019-10-14 23:32:19 +0800

    update1

---
+++ basic_knowledge/StringBufferTest.java
@@ -0,0 +1,14 @@
+public class StringBufferTest{
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(1);
+        
+        sb.append("23");
+        System.out.println(sb.toString());
+        sb.delete(0, 2);
+        System.out.println(sb.toString());
+
+        sb.delete(0, sb.length());
+        System.out.println(sb.toString());
+    }
+}
