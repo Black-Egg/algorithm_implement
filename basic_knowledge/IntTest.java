ca33985c3d324893bc2fd0a1e96f5af0bec99fb4
yeanjiang <yeanjiang11@qq.com>
2019-10-16 19:16:28 +0800

    update

--- basic_knowledge/IntTest.java
+++ basic_knowledge/IntTest.java
@@ -2,6 +2,8 @@ public class IntTest{
     public static void main(String[] args) {
         int b = 1534236469;
         int c = b * 9;
+        Long a = 200L;
+        System.out.println(a * 1 == 200L);
 
     }
 }
--- basic_knowledge/SetTest.java
+++ basic_knowledge/SetTest.java
@@ -3,6 +3,9 @@ import java.util.HashSet;
 public class SetTest{
     public static void main(String[] args){
         Set<Integer> set = new HashSet<>();
+        Integer a = Integer.valueOf(221);
+        Integer b = Integer.valueOf(221);
+        System.out.println(a == b);
     }
 
 }
